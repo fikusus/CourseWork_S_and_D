9c341ed72fe8e5eacf820cef6d8b003d4b153926
danilakuzmuk <danila.kuzmuk@gmail.com>
2020-05-18 21:53:55 +0300

    Some small fixes. V0.0.5

--- CourseWork_S_and_D/View/TaskListForm.cs
+++ CourseWork_S_and_D/View/TaskListForm.cs
@@ -37,7 +37,7 @@ namespace CourseWork_S_and_D
         {
             this.Text = "Завдання на тиждень";
             titleLable.Text = "ЗАВДАННЯ НА ТИЖДЕНЬ";
-            this.taskList.Text = Controller.controller.TasksCalendarText(DateTime.Now, DateTime.Today.AddDays(-(int)DateTime.Today.DayOfWeek + 8).AddTicks(-1););
+            this.taskList.Text = Controller.controller.TasksCalendarText(DateTime.Now, DateTime.Today.AddDays(-(int)DateTime.Today.DayOfWeek + 8).AddTicks(-1));
         }
 
         protected override void OnShown(EventArgs e)
